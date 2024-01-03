921efe1cc63cbcbd15b6a3613ee38b76c07151f6
syed zunair gillani <zunairgillani54@gmail.com>
2024-01-03 11:50:22 +0500

    update

--- src/components/case-studies/CaseStudies.tsx
+++ src/components/case-studies/CaseStudies.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CaseStudiesCard from './CaseStudiesCard'
+import Image from 'next/image'
 
 interface ICaseStudiesItemsProps{
      item:any
@@ -8,7 +9,11 @@ interface ICaseStudiesItemsProps{
 const CaseStudies = () => {
   return (
     <section className='container mx-auto pb-20 px-3 md:px-0'>
-          <h2 className=' text-2xl md:mt-5 md:text-[42px] font-extrabold text-white text-center pb-16'>Case <span className='grad'>Studies</span></h2>
+          <div className='relative'>
+               <h2 className=' text-2xl md:mt-5 md:text-[42px] font-extrabold text-white text-center pb-16'>Case <span className='grad'>Studies</span></h2>
+               <Image src="/images/half-circle.svg" alt="img" width={140} height={140} className='z-[1] absolute md:-top-10 top-0 md:right-16 w-20 md:w-auto right-2'/>
+               <Image src="/images/arrow.svg" alt="img" width={120} height={120} className='z-[1] absolute -top-10 md:-left-24 left-0 w-20 md:w-auto'/>
+          </div>
           <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-7 md:mt-5'>
           {
                [1,2,3,4].slice(0,3).map((item:any,idx:number)=>(
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,17 @@ import Footer from '@/components/footer'
 import Header from '@/components/header'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const {pathname} =  useRouter()
   return <>
-    <Header />
+    {
+      !pathname.includes("/studio") && <Header />
+    }
     <Component {...pageProps} />
-    <Footer />
+    {
+      !pathname.includes("/studio") && <Footer />
+    }
   </>
 }
