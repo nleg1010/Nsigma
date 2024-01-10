f2e7a4994ff0d25a3c4eb79b5a25ca8a8e934433
TutusBOT <barte77pl@gmail.com>
2024-01-10 23:10:33 +0100

    feat: improve the subheader section

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -1,6 +1,7 @@
 import { Variants, motion, useAnimation } from "framer-motion";
 import { useEffect, useState } from "react";
 import CustomLink from "./CustomLink";
+import Image from "next/image";
 
 const variantsLeft: Variants = {
 	initial: {
@@ -79,8 +80,20 @@ function Questions({ data }: QuestionsProps) {
 
 	return (
 		<div className="w-full gap-4 flex items-center justify-center flex-col">
-			<h2 className="text-white mt-5 md:text-6xl text-3xl font-normal">NSigma: Your New Friend To Help You Outperform Competitors with Custom AI and Data Solutions</h2>
-			<CustomLink href="/contact-us" className="font-semibold text-lg my-5">Get Started</CustomLink>
+			<h2 className="text-white mt-5 text-lg md:text-3xl font-normal">
+				<Image
+					src="/images/logo.png"
+					alt="logo"
+					width={187}
+					height={57}
+					className="z-50 -mt-1 inline-block max-w-28 md:max-w-none"
+				/>
+				: Your New Friend To Help You Outperform Competitors with Custom AI and
+				Data Solutions
+			</h2>
+			<CustomLink href="/contact-us" className="font-semibold text-lg my-5">
+				Get Started
+			</CustomLink>
 			<div className="overflow-hidden relative">
 				<span className="opacity-0">{data[offset].title}</span>
 				<motion.div
